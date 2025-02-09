16d41b1652e452e8f1362a1825396cdb4295ea39
Amber <AmberKortier@gmail.com>
2025-02-09 15:16:15 +0100

    Begin of acquisition List

--- AcquisitionDate/Windows/Windows/AcquisitionListWindow.cs
+++ AcquisitionDate/Windows/Windows/AcquisitionListWindow.cs
@@ -3,7 +3,14 @@ using AcquisitionDate.Database.Interfaces;
 using AcquisitionDate.DatableUsers.Interfaces;
 using AcquisitionDate.ImageDatabase.Interfaces;
 using AcquisitionDate.Windows.Components.Image;
+using AcquistionDate.PetNicknames.TranslatorSystem;
+using AcquistionDate.PetNicknames.Windowing.Components.Labels;
+using AcquistionDate.PetNicknames.Windowing.Components;
+using Dalamud.Interface.Utility;
+using ImGuiNET;
 using System.Numerics;
+using Dalamud.Interface;
+using Dalamud.Utility;
 
 namespace AcquisitionDate.Windows.Windows;
 
@@ -19,6 +26,18 @@ internal unsafe class AcquisitionListWindow : AcquisitionWindow
     readonly IDatabase Database;
     readonly IImageDatabase ImageDatabase;
 
+    float BarHeight => 30 * ImGuiHelpers.GlobalScaleSafe;
+
+    bool inUserMode = false;
+    bool lastInUserMode = false;
+    bool isLocalEntry = false;
+
+    string SearchText = string.Empty;
+    string activeSearchText = string.Empty;
+
+    IDatableData? ActiveEntry;
+    IDatableUser? lastUser;
+
     public AcquisitionListWindow(WindowHandler windowHandler, Configuration configuration, IUserList userList, IDatabase database, IImageDatabase imageDatabase) : base(windowHandler, configuration, "Acquisition List")
     {
         Open();
@@ -28,12 +47,207 @@ internal unsafe class AcquisitionListWindow : AcquisitionWindow
         ImageDatabase = imageDatabase;
     }
 
+    public override void OnOpen()
+    {
+        ClearSearchBar();
+        SetUser(UserList.ActiveUser?.Data);
+    }
+
     protected override void OnDraw()
     {
-        foreach (IDatableData data in Database.GetEntries())
+        if (lastUser != UserList.ActiveUser)
         {
-            //PluginHandlers.PluginLog.Debug($"Tries to draw Entry: {data.Name}@{data.HomeworldName}");
-            PlayerImage.Draw(data, ImageDatabase);
+            lastUser = UserList.ActiveUser;
+            SetUser(lastUser?.Data);
+        }
+
+        DrawHeader();
+        DrawSearchbar();
+
+        if (inUserMode)
+        {
+            DrawUserList();
+        }
+        else
+        {
+
+        }
+    }
+
+    void DrawHeader()
+    {
+        if (Listbox.Begin($"##Listbox_{WindowHandler.InternalCounter}", new Vector2(250, 110) * ImGuiHelpers.GlobalScale))
+        {
+            PlayerImage.Draw(ActiveEntry, in ImageDatabase);
+            ImGui.SameLine();
+
+            if (Listbox.Begin($"##Listbox_{WindowHandler.InternalCounter}", ImGui.GetContentRegionAvail()))
+            {
+                TextAligner.Align(TextAlignment.Left);
+
+                float contentAvailableX = ImGui.GetContentRegionAvail().X;
+                Vector2 barSize = new Vector2(contentAvailableX, BarHeight);
+
+                if (ImGui.Button(ActiveEntry?.Name ?? Translator.GetLine("...") + $"##ToggleButtonButton_{WindowHandler.InternalCounter}", barSize))
+                {
+                    PluginHandlers.Framework.Run(() => ToggleUserMode());
+                }
+                if (ImGui.IsItemHovered(ImGuiHoveredFlags.AllowWhenDisabled))
+                {
+                    if (!inUserMode)
+                    {
+                        ImGui.SetTooltip(Translator.GetLine("PetList.UserList"));
+                    }
+                    else
+                    {
+                        ImGui.SetTooltip(Translator.GetLine("PetList.Title"));
+                    }
+                }
+                BasicLabel.Draw(ActiveEntry?.HomeworldName ?? Translator.GetLine("..."), barSize);
+                BasicLabel.Draw(ActiveEntry?.LodestoneID?.ToString() ?? Translator.GetLine("..."), barSize);
+
+                TextAligner.PopAlignment();
+                Listbox.End();
+            }
+
+            Listbox.End();
+        }
+
+        ImGui.SameLine();
+
+        if (Listbox.Begin($"##Listbox_{WindowHandler.InternalCounter}", new Vector2(ImGui.GetContentRegionAvail().X, 110 * ImGuiHelpers.GlobalScale)))
+        {
+            Listbox.End();
+        }
+    }
+
+    void DrawSearchbar()
+    {
+        if (Listbox.Begin($"##Listbox_{WindowHandler.InternalCounter}", new Vector2(ImGui.GetContentRegionAvail().X, 30 * ImGuiHelpers.GlobalScale)))
+        {
+            ImGuiStylePtr style = ImGui.GetStyle();
+            float buttSize = ImGui.GetContentRegionAvail().Y;
+
+            bool clicked = false;
+
+            if (ImGui.InputTextMultiline($"##InputText_{WindowHandler.InternalCounter}", ref SearchText, 64, new Vector2(ImGui.GetContentRegionAvail().X - buttSize - style.FramePadding.X, buttSize), ImGuiInputTextFlags.CtrlEnterForNewLine | ImGuiInputTextFlags.EnterReturnsTrue))
+            {
+                clicked |= true;
+            }
+
+            SearchText = SearchText.Replace("\n", string.Empty);
+
+            ImGui.SameLine();
+
+            ImGui.PushFont(UiBuilder.IconFont);
+
+            if (ImGui.Button($"{FontAwesomeIcon.Search.ToIconString()}##Search_{WindowHandler.InternalCounter}", new Vector2(buttSize, buttSize)))
+            {
+                clicked |= true;
+            }
+
+            ImGui.PopFont();
+
+            if (ImGui.IsItemHovered())
+            {
+                ImGui.SetTooltip(Translator.GetLine("Search"));
+            }
+
+            if (clicked)
+            {
+                activeSearchText = SearchText;
+                PluginHandlers.Framework.Run(() => SetUser(ActiveEntry));
+            }
+
+            Listbox.End();
+        }
+    }
+
+    void DrawUserList()
+    {
+        if (Listbox.Begin($"##Listbox_{WindowHandler.InternalCounter}", ImGui.GetContentRegionAvail()))
+        {
+            foreach (IDatableData data in Database.GetEntries())
+            {
+                if (!SearchText.IsNullOrWhitespace())
+                {
+                    bool valid =
+                        data.Name           .Contains(SearchText) ||
+                        data.HomeworldName  .Contains(SearchText);
+                    
+                    if (!valid) continue;
+                }
+
+                if (Listbox.Begin($"##Listbox_{WindowHandler.InternalCounter}", new Vector2(ImGui.GetContentRegionAvail().X, 110 * ImGuiHelpers.GlobalScale)))
+                {
+                    PlayerImage.Draw(data, in ImageDatabase);
+                    ImGui.SameLine();
+
+                    if (Listbox.Begin($"##Listbox_{WindowHandler.InternalCounter}", ImGui.GetContentRegionAvail()))
+                    {
+                        if (LabledLabel.DrawButton("Username:", data.Name, new Vector2(ImGui.GetContentRegionAvail().X, BarHeight)))
+                        {
+                            PluginHandlers.Framework.Run(() =>
+                            {
+                                ToggleUserMode();
+                                SetUser(data);
+                            });
+                        }
+                        LabledLabel.Draw("Homeworld:", data.HomeworldName, new Vector2(ImGui.GetContentRegionAvail().X, BarHeight));
+                        LabledLabel.Draw("Lodestone ID:", data.LodestoneID?.ToString() ?? Translator.GetLine("..."), new Vector2(ImGui.GetContentRegionAvail().X, BarHeight));
+
+                        Listbox.End();
+                    }
+                    Listbox.End();
+                }
+            }
+
+            Listbox.End();
+        }
+    }
+
+    void ClearSearchBar()
+    {
+        SearchText = string.Empty;
+        activeSearchText = string.Empty;
+    }
+
+    void ToggleUserMode()
+    {
+        inUserMode = !inUserMode;
+
+        if (!inUserMode && UserList.ActiveUser != null)
+        {
+            ActiveEntry = UserList.ActiveUser?.Data;
+        }
+
+        SetUser(ActiveEntry);
+    }
+
+    public void SetUser(IDatableData? entry)
+    {
+        bool completeUserChange = ActiveEntry != entry;
+
+        isLocalEntry = HandleIfLocalEntry(entry);
+
+        ActiveEntry = entry;
+
+        if (lastInUserMode != inUserMode || completeUserChange)
+        {
+            lastInUserMode = inUserMode;
+            ClearSearchBar();
+        }
+    }
+
+    bool HandleIfLocalEntry(IDatableData? entry)
+    {
+        if (UserList.ActiveUser != null && entry != null)
+        {
+            return UserList.ActiveUser.ContentID == entry.ContentID;
+        }
+        else
+        {
+            return false;
         }
     }
 
